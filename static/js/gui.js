1f932000d1ce3ca9a76d5725e5243ed78d4a03da
Mark Ndipenoch <G00352031@GMIT.IE>
2019-11-08 16:54:49 +0000

    Gui file added and variables created.

---
+++ static/js/gui.js
@@ -0,0 +1,23 @@
+//---------------
+// Adapted from https://bensonruan.com/handwritten-digit-recognition-with-tensorflow-js/
+//---------------
+
+//Variables
+let model;
+var canvasWidth = 150;
+var canvasHeight = 150;
+var canvasStrokeStyle = "white";
+var canvasLineJoin = "round";
+var canvasLineWidth = 10;
+var canvasBackgroundColor = "black";
+var canvasId = "canvas";
+var clickX = new Array();
+var clickY = new Array();
+var clickD = new Array();
+var movingMouse;
+var prevX = 0;
+var currX = 0;
+var prevY = 0;
+var currY = 0;
+var canvasBox = document.getElementById('canvas_box');
+var canvas    = document.createElement("canvas");
